e5b935d3ae08252c63c68dd4f675e8b4818a1e61
Maurice Becnel <mauricebecnel400@gmail.com>
2023-08-29 23:44:34 -0500

    another small styling tweak

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center gap-20 p-10 mx-40 p-20 max-w-screen-xl">
+    <main className="flex flex-col items-center gap-20 p-10 mx-40 p-20">
       <div className="w-full flex justify-between mx-20 gap-10 items-center flex-col lg:flex-row">
-        <div className="flex flex-col justify-center items-center md:items-start lg:items-start">
+        <div className="flex flex-col justify-center items-center md:items-center lg:items-start">
           <h1 className="text-5xl font-bold mt-4 mb-4">Maurice&nbsp;Becnel</h1>
           <div className="flex gap-2 items-center justify-center">
             <p className="text-xl">Associate&nbsp;Fullstack&nbsp;Developer</p> 

3c28120a7b4d82fbcbc5e0f83c3ef83fcbcc9588
Maurice Becnel <mauricebecnel400@gmail.com>
2023-09-03 15:59:37 -0500

    updated text sizing for mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <div className="flex flex-col justify-center items-center md:items-center lg:items-start">
               <h1 className="text-5xl font-bold mt-4 mb-4">Maurice&nbsp;Becnel</h1>
               <div className="flex gap-2 items-center justify-center">
-                <p className="text-xl">Associate&nbsp;Fullstack&nbsp;Developer</p> 
+                <p className="text-md md:text-xl lg:text-xl">Associate&nbsp;Fullstack&nbsp;Developer</p> 
                 <a className="inline text-xl text-secondary" href="https://www.goldmansachs.com">@Goldman&nbsp;Sachs</a>
               </div>
             </div>
@@ -23,10 +23,10 @@ export default function Home() {
             </div>
           </div>
           <div className="flex justify-center">
-          <h2 className="text-4xl font-bold"> 
+          <h2 className="text-2xl font-bold md:text-4xl lg:text-4xl"> 
           Dedicated to improving digital experiences one
           <pre className="inline p-2 text-xl inline">
-            <code className="bg-secondary px-1 text-3xl">npm install</code> 
+            <code className="bg-secondary px-1 text-xl md:text-3xl lg:text-3xl">npm install</code> 
           </pre>
           at a time.
           </h2>

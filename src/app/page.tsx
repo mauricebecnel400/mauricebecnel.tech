9c10ee6653b02e98f306034aada7f7e02c3c51e6
Maurice Becnel <mauricebecnel400@gmail.com>
2023-09-03 16:11:00 -0500

    fixed overflow on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center gap-20 p-10 mx-40 p-20">
       <div className="max-w-6xl flex flex-col justify-center items-center gap-20">
-          <div className="w-full flex justify-between mx-20 gap-10 items-center flex-col lg:flex-row">
+          <div className="w-full flex justify-between gap-10 items-center flex-col lg:flex-row">
             <div className="flex flex-col justify-center items-center md:items-center lg:items-start">
               <h1 className="text-5xl font-bold mt-4 mb-4">Maurice&nbsp;Becnel</h1>
               <div className="flex gap-2 items-center justify-center">
@@ -22,14 +22,14 @@ export default function Home() {
                 />
             </div>
           </div>
-          <div className="flex justify-center">
-          <h2 className="text-2xl font-bold md:text-4xl lg:text-4xl"> 
-          Dedicated to improving digital experiences one
-          <pre className="inline p-2 text-xl inline">
-            <code className="bg-secondary px-1 text-xl md:text-3xl lg:text-3xl">npm install</code> 
-          </pre>
-          at a time.
-          </h2>
+          <div className="flex justify-center text-center md:text-left lg:text-left">
+            <h2 className="text-xl font-bold md:text-4xl lg:text-4xl"> 
+              Dedicated to improving digital experiences one
+              <pre className="inline p-2 text-xl inline">
+                <code className="bg-secondary px-1 text-xl md:text-3xl lg:text-3xl">npm install</code> 
+              </pre>
+              at a time.
+            </h2>
         </div>
       </div>
     </main>
